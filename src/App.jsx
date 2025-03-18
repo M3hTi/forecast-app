3f4f3cf374e01cb855710c8e9678ead5cf7482ff
M3hTi <mehdinasir502@yahoo.com>
2025-03-18 14:59:23 +0330

    feat: enhance weather application with error handling and loading states; add ErrorMessage component for displaying errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,46 @@ import { useState } from "react";
 import Form from "./Form";
 import ShowWeather from "./ShowWeather";
 import Loader from "./Loader";
+import ErrorMessage from "./ErrorMessage";
 
-const apiKey = "a084759fbe9642c6987115928251703";
+const apiKey = "693706a377c449ff848110424251803";
 
 function App() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   function handleSubmitForm(e) {
     e.preventDefault();
     if (!query) return;
-    setIsLoading(true)
+    setIsLoading(true);
     fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${query}`)
       .then((res) => {
-        if (!res.ok) throw new Error(`Error: ${res.statusText}`);
+        console.log(res)
+        if (!res.ok) throw new Error(`something wrong!`);
         return res.json();
       })
       .then((data) => {
         console.log(data);
+
         setWeather(data);
-        setIsLoading(false)
+        setIsLoading(false);
       })
-      .catch((err) => console.log(err));
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+        setIsLoading(false)
+      });
 
     setQuery("");
   }
   return (
-    <div>
+    <div className="app">
       <Form onHandler={handleSubmitForm} query={query} setQuery={setQuery} />
-      {isLoading ? <Loader /> : <ShowWeather weatherObj={weather} />}
+      {isLoading && <Loader />}
+      {!isLoading && !error && <ShowWeather weatherObj={weather} />}
+      {error && <ErrorMessage msg={error} />}
     </div>
   );
 }
---
+++ src/ErrorMessage.jsx
@@ -0,0 +1,5 @@
+function ErrorMessage({ msg }) {
+  return <p className="Error">{msg}</p>;
+}
+
+export default ErrorMessage;
